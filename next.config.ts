37c610c7191e57494640d413adc8a1bc2e3178ad
Bhautik <solankibhautik1702@gmail.com>
2025-05-18 23:05:24 +0530

    fixing sum shit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['undici', '@firebase/auth'],
+  webpack: (config, { isServer }) => {
+    // This will fix the undici issue by forcing browser implementation
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        // Force browser implementations for problematic modules
+        'undici': false,
+        '@firebase/auth': '@firebase/auth/dist/esm2017/index.browser.js',
+        'firebase/auth': 'firebase/auth/dist/index.esm.js',
+      };
+    }
+    return config;
+  },
+  // Add all the problematic packages to be transpiled
+  transpilePackages: ['undici', '@firebase/auth', 'firebase'],
+  // If needed, you can also disable server components for specific routes
+  experimental: {
+    // If you have auth-related pages, consider excluding them from Server Components
+    // appDir: true,
+    // serverComponentsExternalPackages: ['firebase', '@firebase/auth'],
+  },
 };
 
 module.exports = nextConfig;
