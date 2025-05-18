17e1081a5e213b6416b0908152b85ffaa2a80d56
Bhautik <solankibhautik1702@gmail.com>
2025-05-18 22:39:02 +0530

    updated configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['undici'],
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'undici': path.resolve(__dirname, 'node_modules/undici/lib/index.cjs'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
